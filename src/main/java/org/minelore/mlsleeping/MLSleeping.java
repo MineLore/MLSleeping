b4d7d57fae0d97d62e6020044eba90af7a544abc
CaHeK2009 <alex.chernya20@gmail.com>
2025-08-21 20:36:22 +0300

    Initial commit

---
+++ src/main/java/org/minelore/mlsleeping/MLSleeping.java
@@ -0,0 +1,17 @@
+package org.minelore.mlsleeping;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MLSleeping extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
